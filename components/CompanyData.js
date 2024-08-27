824e8935c0254a16ed508c315a9c4b7f0b035a99
Taha Ansari <taha.m.ansari@gmail.com>
2024-08-27 14:33:32 +0530

    wip

---
+++ components/CompanyData.js
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+const CompanyData = () => {
+  return (
+    <>
+      <Text>Name : Schbang</Text>
+      <Text>Total Employee : 2250</Text>
+      <Text>Sector : Digital Marketing</Text>
+    </>
+  );
+};
+
+export default CompanyData;
